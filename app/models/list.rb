b2c10cdee8c5a74a3c6d8d5aa99ac2c2b6ca5ae3
Jonathan Parmley <jonathanparmley@gmail.com>
2021-04-20 18:51:55 +0200

    added view for new list

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,5 @@
 class List < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
   has_many :bookmarks, dependent: :destroy
+  validates :name, uniqueness: true, presence: true
   has_many :movies, through: :bookmarks
 end
